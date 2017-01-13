123f0b691e8dc67880d896b609d8f6354716fac8
Micah <micahmartin@gmail.com>
2017-01-13 11:05:48 -0700

    adjusting url paths

---
+++ _posts/2017-01-12-taking-inventory.md
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  "Taking Inventory
+"
+date:   2017-01-13 
+categories: onex
+---
+
+Today was a great day!
---
+++ bin/new.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby -w
+
+puts "What's the title?"
+title = gets
+
+slug = title.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+date_str = Time.now.strftime("%Y-%m-%d")
+
+post = <<END
+---
+layout: post
+title:  "#{title}"
+date:   #{date_str} 
+categories: onex
+---
+
+Today was a great day!
+END
+
+post_name = "#{date_str}-#{slug}.md"
+post_filename = "_posts/#{post_name}"
+puts "Creating post: #{File::absolute_path(post_filename)}"
+File.open(post_filename, "w") do |f|
+  f.write(post)
+end
+
+img_filename = "img/#{date_str}"
+puts "Creating img dir: #{File::absolute_path(img_filename)}"
+Dir::mkdir(img_filename)
+
+
+
+
+
+
+
